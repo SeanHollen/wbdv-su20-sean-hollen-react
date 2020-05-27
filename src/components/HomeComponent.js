1f4e451805f64a7128ecb9796324c57a5da12bd3
sean.hollen <hollen.s@husky.neu.edu>
2020-05-26 22:09:41 -0400

    setup changes

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -3,22 +3,12 @@ import {Link} from "react-router-dom";
 
 const HomeComponent = () =>
   <div>
-    <h2>Home</h2>
     <div className="list-group">
       <Link className="list-group-item" to='/editor'>
         Course Editor
       </Link>
       <Link className="list-group-item" to='/table/courses'>
         Course List
-      </Link>
-      <Link className="list-group-item" to='/login'>
-        Login
-      </Link>
-      <Link className="list-group-item" to='/register'>
-        Registration
-      </Link>
-      <Link className="list-group-item" to='/profile'>
-        Profile
       </Link>
     </div>
   </div>
