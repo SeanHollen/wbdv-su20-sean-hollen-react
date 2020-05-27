173e375a0fbd51352176952f167e7ebbd9c63a11
sean.hollen <hollen.s@husky.neu.edu>
2020-05-26 21:46:44 -0400

    init

---
+++ src/components/WhiteBoard.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import CourseListContainer from "../containers/CourseListContainer";
+import CourseEditor from "./CourseEditor";
+import {BrowserRouter, Route} from "react-router-dom";
+import HomeComponent from "./HomeComponent";
+import LoginComponent from "./LogicComponent";
+
+class WhiteBoard extends React.Component {
+  render() {
+    return(
+      <BrowserRouter>
+        <div>
+
+          <Route
+            path='/'
+            exact={true}
+            component={HomeComponent}
+          />
+
+          <Route
+            path='/courses'
+            exact={true}
+            component={CourseListContainer}/>
+
+          <Route
+            path='/:layout/courses'
+            exact={true}
+            component={CourseListContainer}/>
+
+          <Route
+            path='/editor'
+            exact={true}
+            component={CourseEditor}/>
+
+        </div>
+      </BrowserRouter>
+    )
+  }
+}
+
+export default WhiteBoard
