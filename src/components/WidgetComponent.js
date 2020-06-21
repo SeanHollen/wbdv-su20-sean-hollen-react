b9804ab9badd1a1fb6fbbb9ef1f88970c98c308a
sean.hollen <hollen.s@husky.neu.edu>
2020-06-21 15:01:33 -0400

    more edits

--- src/components/WidgetComponent.js
+++ src/components/WidgetComponent.js
@@ -4,17 +4,12 @@ export default class WidgetComponent extends React.Component {
 
     state = {
         style: this.props.widget.style,
-        text: this.props.widget.text
-    }
-
-    list() {
-        return this.props.text.split(" ").map(
-            item => <li>{item}</li>
-        )
+        text: this.props.widget.text,
+        name: this.props.widget.name,
+        type: this.props.widget.type
     }
 
     setText(text) {
-        console.log("setting text " + text);
         this.props.widget.text = text;
         this.setState({ text: text });
     }
@@ -26,6 +21,12 @@ export default class WidgetComponent extends React.Component {
 
     setName(name) {
         this.props.widget.name = name;
+        this.setState({ name: name });
+    }
+
+    setType(type) {
+        this.props.widget.type = type;
+        this.setState({ type: type });
     }
 
     render() {
@@ -37,45 +38,40 @@ export default class WidgetComponent extends React.Component {
                 <button onClick={() =>
                     this.props.deleteWidget(this.props.widget.id)}
                     className="btn btn-success">X</button>
-                <select className="btn btn-success" onChange={(e) => this.setStyle(e.value)}>
-                    <option value="list">List</option>
-                    <option value="heading">Heading</option>
+                <select className="btn btn-success" onChange={(e) => this.setType(e.target.value)}>
+                    <option value=""></option>
+                    <option value="HEADING">Heading</option>
+                    <option value="PARAGRAPH">Paragraph</option>
                 </select>
             </div>
-
-            <br></br><br></br>
+            <br></br>
             <input className="form-control" placeholder="text" value={this.props.widget.text}
-                onInput={(e) => this.setText(e.target.value)}></input><br></br>
+                onChange={(e) => this.setText(e.target.value)} /><br />
             {
-                this.props.widget.type == "PARAGRAPH" &&
-                <select className="form-control" onChange={(e) => this.setStyle(e.target.value)}>
+                this.props.widget.type == "HEADING" &&
+                <div><select className="form-control" onChange={(e) => this.setStyle(e.target.value)}>
+                    <option value=""></option>
                     <option value="H3">H3</option>
                     <option value="H2">H2</option>
                     <option value="H1">H1</option>
                 </select>
+                <br></br></div>
             }
-            {
-                this.props.widget.type == "LIST" &&
-                <select className="form-control">
-                    <option value="list">Unordered List</option>
-                    <option value="heading">Ordered List</option>
-                </select>
-            }
-            <br></br><input className="form-control" placeholder="widget name"
-                >{this.props.widget.name}</input><br></br>
+            <input className="form-control" placeholder="widget name"
+                onChange={(e) => this.setName(e.target.value)} value={this.props.widget.name}></input><br></br>
 
             {this.props.preview && <h3>Preview:</h3>}
 
-            {(this.props.preview && this.props.widget.type == "PARAGRAPH") &&
+            {(this.props.preview && this.props.widget.type == "HEADING") &&
                 <div>
-                    {this.props.widget.style == "H1" && <h1>a{this.props.widget.text}</h1>}
-                    {this.props.widget.style == "H2" && <h2>a{this.props.widget.text}</h2>}
-                    {this.props.widget.style == "H3" && <h3>a{this.props.widget.text}</h3>}
+                    {this.props.widget.style == "H1" && <h1>{this.props.widget.text}</h1>}
+                    {this.props.widget.style == "H2" && <h2>{this.props.widget.text}</h2>}
+                    {this.props.widget.style == "H3" && <h3>{this.props.widget.text}</h3>}
                 </div>}
-            {(this.props.preview && this.props.widget.type == "LIST") &&
-                <ul>
-                    {this.list()}
-                </ul>}
+            {
+                (this.props.preview && this.props.widget.type == "PARAGRAPH") &&
+                <p>{this.props.widget.text}</p>
+            }
         </div>
     }
 }
--- src/components/WidgetListComponent.js
+++ src/components/WidgetListComponent.js
@@ -6,31 +6,39 @@ export default class WidgetListComponent extends React.Component {
     state = {
         path: this.props.params.path,
         updated: [],
-        added: [],
         deleted: [],
-        preview: true
+        added: [],
+        preview: false
     }
+
     constructor(props) {
         super(props);
     }
+
     componentDidMount() {
-        this.props.findWidgetsForTopic("Topic1"); 
+        this.props.findWidgetsForTopic("Topic1");
     }
 
     delWidget(widget) {
-        this.setState(...this.state.deleted, widget);
+        console.log("test"); 
+        console.log(this.state); 
+        this.setState({deleted: this.state.deleted.concat(widget)});
     }
 
     addWidget() {
-
+        let newWidget = { id: Math.random() * 1000, name: "", type: "PARAGRAPH", topic: "Topic1", style: "", text: "" };
+        this.props.widgets.push(newWidget);
+        this.setState({ added: this.state.added.concat(newWidget) });
     }
 
     save() {
-
+        // for (let i = 0; i < this.props.widgets; i++) {
+        //     this.props.updateWidget(this.props.widgets[i].id, this.props.widgets[i]);
+        // }
     }
 
     toggle() {
-        this.setState({preview: !this.state.preview}); 
+        this.setState({ preview: !this.state.preview });
     }
 
     render() {
@@ -38,20 +46,21 @@ export default class WidgetListComponent extends React.Component {
             <div class="container">
                 <button onClick={this.save}
                     className="btn btn-success">Save</button>
-                <button onClick={() => null}
+                <button onClick={() => this.addWidget()}
                     className="btn btn-success">Add Widget</button>
                 <div style={{ display: 'inline-block' }} class="custom-control custom-switch">
-                    <input type="checkbox" class="custom-control-input" id="customSwitches" 
-                    onInput={() => this.toggle()}/>
+                    <input type="checkbox" class="custom-control-input" id="customSwitches"
+                        onInput={() => this.toggle()} />
                     <label class="custom-control-label" for="customSwitches">Preview</label>
                 </div>
                 {
                     this.props.widgets.map(
                         widget => <div>
+                            <br></br>
                             <WidgetComponent
                                 widget={widget}
-                                deleteWidget={this.props.delWidget}
-                                preview={this.state.preview} /><br></br>
+                                deleteWidget={this.delWidget}
+                                preview={this.state.preview} />
                         </div>
                     )
                 }
